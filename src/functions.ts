805905f61ddb9c8abc4f3c2018ff1929ca152026
CodewithClouds <74655404+CodewithClouds@users.noreply.github.com>
2024-02-24 19:56:32 +0000

    adeed the hi and bye function

--- src/functions.ts
+++ src/functions.ts
@@ -10,3 +10,24 @@ export function SayHello({firstName, lastName, age}: SayHelloProps) {
         console.log(`Your age is: ${age}`)
     }
 }
+
+export function SayHi({firstName, lastName, age}: SayHelloProps) {
+    console.log('Hi!')
+    console.log(`Your first name is: ${firstName}`)
+    if (lastName) {
+        console.log(`Your last name is: ${lastName}`)
+    }
+    if (age) {
+        console.log(`Your age is: ${age}`)
+    }
+}
+export function SayBye({firstName, lastName, age}: SayHelloProps) {
+    console.log('Bye!')
+    console.log(`Your first name is: ${firstName}`)
+    if (lastName) {
+        console.log(`Your last name is: ${lastName}`)
+    }
+    if (age) {
+        console.log(`Your age is: ${age}`)
+    }
+}
